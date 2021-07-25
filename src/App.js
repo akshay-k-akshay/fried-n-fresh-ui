f0abaab6cb8bad35700e81c1eaf8b6e54715b110
Akshay K <akshayk.info01@gmail.com>
2021-07-25 21:48:03 +0530

    cart page integrated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 import Header from './components/Header';
 import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/" component={HomePage} />
           <Route exact path="/products" component={Products} />
           <Route exact path="/products/:id" component={ProductDetails} />
+          <Route exact path="/cart" component={Cart} />
           <Route exact path="/signin" component={SignIn} />
           <Route exact path="/signup" component={SignUp} />
 
