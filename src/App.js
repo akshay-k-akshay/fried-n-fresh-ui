7c22d25546f4a916e8820db7e10c3859ff21623c
Akshay K <akshayk.info01@gmail.com>
2021-07-25 12:09:15 +0530

    all pages added to routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import HomePage from './pages/HomePage';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import Header from './components/Header';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div>
       <Router>
-        <Switch></Switch>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/products/:id" component={ProductDetails} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+
+          <Route component={NotFound} />
+        </Switch>
       </Router>
     </div>
   );
