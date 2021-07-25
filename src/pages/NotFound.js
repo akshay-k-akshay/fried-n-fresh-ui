8bcf7b6b0951b0b6fa9ea882265961c0c7c11c17
Akshay K <akshayk.info01@gmail.com>
2021-07-25 12:07:41 +0530

    404 page added

---
+++ src/pages/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div>
+      <div className="login">
+        <div className="container">
+          <h2>
+            <b>404 </b>
+            Page Not Found
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
