ff3317af36c24a4d36b357052aceb1a91f24e200
Akshay K <akshayk.info01@gmail.com>
2021-07-25 10:35:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch></Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
